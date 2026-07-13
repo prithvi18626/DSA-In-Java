a385ac78c943a13d60199d520ed95ad3c7e322db
Prithvi <auteprithviraj@gmail.com>
2026-07-13 09:56:00 +0530

    Print name in java

---
+++ 01-Java-Basics/PrintName.java
@@ -0,0 +1,7 @@
+// A simple Java program to print your name
+public class PrintName {
+    public static void main(String[] args) {
+        // Replace "Your Name" with your actual name
+        System.out.println("Prithviraj Sandeep Aute");
+    }
+}
