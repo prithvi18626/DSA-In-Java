de32535791a18835c2398cfaaecf0a2185d49742
Prithvi <prithvirajaute1@gmail.com>
2026-07-14 08:55:26 +0530

    .

---
+++ 01-Java-Basics/02-Variables/VariableInitialization.java
@@ -0,0 +1,20 @@
+public class VariableInitialization {
+    public static void main(String[] args) {
+
+        // Variable Declaration with Initialization
+        int age = 20;
+        double salary = 45000.50;
+        char grade = 'A';
+        boolean isStudent = true;
+        long population = 1450000000L;
+        float percentage = 89.5f;
+
+        // Displaying the values
+        System.out.println("Age: " + age);
+        System.out.println("Salary: " + salary);
+        System.out.println("Grade: " + grade);
+        System.out.println("Is Student: " + isStudent);
+        System.out.println("Population: " + population);
+        System.out.println("Percentage: " + percentage);
+    }
+}
