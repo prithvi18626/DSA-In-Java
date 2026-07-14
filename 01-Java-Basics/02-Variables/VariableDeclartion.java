dd4be1076130f0b14f2cc21a1d6b517c79f97dd8
Prithvi <auteprithviraj@gmail.com>
2026-07-14 08:22:30 +0530

    Add variable declaration and initialization examples

---
+++ 01-Java-Basics/02-Variables/VariableDeclartion.java
@@ -0,0 +1,24 @@
+public class VariableDeclartion{
+  public static void main(String[] args) {
+    int age;
+    double salary;
+    char grade; 
+    long population;
+    boolean isStudent;
+    float percentage;
+
+    age = 20;
+    salary = 65000;
+    grade = 'A';
+    population = 1450000000L;
+    isStudent = true;
+    percentage =90.7f;
+    
+    System.out.println("Age: " + age);
+    System.out.println("Salary: " + salary);
+    System.out.println("Grade: " + grade);
+    System.out.println("Is Student: " + isStudent);
+    System.out.println("Population: " + population);
+    System.out.println("Percentage: " + percentage);
+    }
+  }
