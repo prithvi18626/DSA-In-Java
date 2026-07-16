688e2e8f39b0b8bdc38bac58e8d758749a56e9d2
Prithvi <prithvirajaute1@gmail.com>
2026-07-16 07:00:40 +0530

    Java program for finding perimeter and area of the rectangle

---
+++ 01-Java-Basics/02-Variables/AreaandPerimeterofRectangle.java
@@ -0,0 +1,13 @@
+public class AreaandPerimeterofRectangle {
+  public  static void main(String[] args){
+
+    int length = 22;
+    int breadth = 11;
+
+    int perimeter = 2*(length + breadth);
+    int area = length *breadth;
+
+    System.out.println("The perimeter and the area of the rectangle is "+perimeter + " and " +area + " respectively");
+
+  }
+}
