09ae4906cabbcdbe5ba9167d54d988191ff8388b
Prithvi <prithvirajaute1@gmail.com>
2026-07-16 06:52:58 +0530

    Program to find the Area of Circle

---
+++ 01-Java-Basics/02-Variables/AreaofCircle.java
@@ -0,0 +1,9 @@
+public class AreaofCircle{
+  public static void main(String[] args){
+
+    int radius = 12;
+    double Area = 3.14 * radius * radius;
+    
+    System.out.println("The are of the Circle is " +Area);
+  }
+}
