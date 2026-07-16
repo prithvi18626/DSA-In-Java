efaaddb9aab7824699369b960a24171a09d17158
Prithvi <prithvirajaute1@gmail.com>
2026-07-16 08:04:52 +0530

    Size and Range of various DataTypes in Jva

---
+++ 01-Java-Basics/03-DataTypes/SizeandRange.java
@@ -0,0 +1,41 @@
+public class SizeandRange {
+  public static void main(String[] args){
+
+    System.out.println("Byte");
+    System.out.println("Size: " + Byte.BYTES + " bytes");
+    System.out.println("Range: " +Byte.MIN_VALUE + " to " + Byte.MAX_VALUE);
+    System.out.println();
+
+        System.out.println("Short");
+    System.out.println("Size: " + Short.BYTES + " bytes");
+    System.out.println("Range: " +Short.MIN_VALUE + " to " + Short.MAX_VALUE);
+    System.out.println();
+
+        System.out.println("Integer");
+    System.out.println("Size: " + Integer.BYTES + " bytes");
+    System.out.println("Range: " +Integer.MIN_VALUE + " to " + Integer.MAX_VALUE);
+    System.out.println();
+
+        System.out.println("Long");
+    System.out.println("Size: " + Long.BYTES + " bytes");
+    System.out.println("Range: " +Long.MIN_VALUE + " to " + Long.MAX_VALUE);
+    System.out.println();
+
+        System.out.println("Float");
+    System.out.println("Size: " + Float.BYTES + " bytes");
+    System.out.println("Range: " +Float.MIN_VALUE + " to " + Float.MAX_VALUE);
+    System.out.println();
+
+        System.out.println("Double");
+    System.out.println("Size: " + Double.BYTES + " bytes");
+    System.out.println("Range: " +Double.MIN_VALUE + " to " + Double.MAX_VALUE);
+    System.out.println();
+
+    System.out.println("Character");
+    System.out.println("Size: " + Character.BYTES + " bytes");
+    System.out.println("Range: " +Character.MIN_VALUE + " to " + Double.MAX_VALUE );
+    System.out.println();
+
+    System.out.println();
+  }
+}
