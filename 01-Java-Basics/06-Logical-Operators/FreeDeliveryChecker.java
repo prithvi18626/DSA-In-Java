bdb254ce7cbefc5e8c5f3fa1ff575ef1147b4bd8
Prithvi <prithvirajaute1@gmail.com>
2026-09-26 03:14:51 +0530

    FreeDeliveryChecker

--- 01-Java-Basics/06-Logical-Operators/FreeDeliveryChecker.java
+++ 01-Java-Basics/06-Logical-Operators/FreeDeliveryChecker.java
@@ -1,22 +1,23 @@
 import java.util.Scanner;
 
-public class FreeDeliveryChecker{
+public class FreeDeliveryChecker {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
-    Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
-    System.out.println("Enter order ammount");
-    double orderAmount = scanner.nextDouble();
+        System.out.print("Enter order amount: ");
+        double orderAmount = scanner.nextDouble();
 
-    System.out.println("Is location serviceable");
-    boolean isServiceable = scanner.nextBoolean();
+        System.out.print("Is location serviceable (true/false): ");
+        boolean isServiceable = scanner.nextBoolean();
 
-    if(orderAmount >= 500 && isServiceable == true){
-      System.out.println("Free delivery available: true/ false");
+        if (orderAmount >= 500 && isServiceable) {
+            System.out.println("Free delivery available.");
+        } else {
+            System.out.println("Delivery charges applicable.");
+        }
 
+        scanner.close();
     }
-    else
-      System.out.println("Delivery charges applicable75");
-  }
 }
